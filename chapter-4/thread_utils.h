f05b5fc1a1410405d0841817947d6e44042a483b
Noah <noahharris.000@gmail.com>
2023-11-11 18:44:46 -0600

    Chapter 4 - Memory Pool

--- chapter-4/thread_utils.h
+++ chapter-4/thread_utils.h
@@ -9,7 +9,7 @@
 
 namespace common {
     inline auto setThreadCore(int core_id) noexcept {
-        cpu_set_t const;
+        cpu_set_t cpuset;
         CPU_ZERO(&cpuset);
         CPU_SET(core_id, &cpuset);
         return (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) == 0);
