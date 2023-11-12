e2ff96ced672a9738338810fce6cd907ecef3301
Noah <noahharris.000@gmail.com>
2023-11-11 18:21:26 -0600

    Chapter 4 - Thread Setting

---
+++ chapter-4/macros.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <cstring>
+
+// __builtin_expect provide branching hints as branching is expensive. Likely specifices condition is likely to be true
+// unlikely specifies the opposite - used as [[likely]] & [[unlikely]]
+#define LIKELY(x) __builtin_expect(!!(x), 1)
+#define UNLIKELY(x) __builtin_expect(!!(x), 0)
+
+inline auto ASSERT(bool cond, const std::string& msg)
+    noexcept { 
+        if (UNLIKELY(!cond)) {
+            std::cerr << msg << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+
+inline auto FATAL(const std::string& msg) noexcept {
+    std::cerr << msg << std::endl;
+    exit(EXIT_FAILURE);
+}
